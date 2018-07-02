094146456ca98d7eb58bce960714b9b69f53727d
HAMISH <constatcopywriting@gmail.com>
2018-07-02 21:50:55 +1200

    Heroku working with hello world express app. Readded API call files to test with Heroku.

--- APIs/flightAwareAPI.js
+++ APIs/flightAwareAPI.js
@@ -1,43 +1,43 @@
-// const password = require('../details.js').key
-// const username = require('../details.js').username
-// const axios = require('axios')
-//
-// const auth = {
-//   username: username,
-//   password: password
-// }
-//
-// const queryAirportScheduled = async (airport, resultCount, filter) => {
-//   const url = 'http://flightxml.flightaware.com/json/FlightXML2/' + 'Scheduled'
-//   const options = {
-//     url: url,
-//     params: {
-//       airport: airport,
-//       howMany:resultCount,
-//       filter: filter
-//     },
-//     auth
-//   }
-//   const result = await axios(options)
-//   return result.data.ScheduledResult.scheduled
-// }
-//
-// const queryFlightInfo = async (tailNumber, resultCount) => {
-//   const url = 'http://flightxml.flightaware.com/json/FlightXML2/' + 'FlightInfoEx'
-//   const options = {
-//     url: url,
-//     params: {
-//       ident: tailNumber,
-//       howMany:resultCount,
-//       offset: 0
-//     },
-//     auth
-//   }
-//   const result = await axios(options)
-//   return result.data.FlightInfoExResult.flights
-// }
-//
-// module.exports = {
-//   queryFlightInfo: queryFlightInfo,
-//   queryAirportScheduled: queryAirportScheduled
-// }
+const password = require('../details.js').key
+const username = require('../details.js').username
+const axios = require('axios')
+
+const auth = {
+  username: username,
+  password: password
+}
+
+const queryAirportScheduled = async (airport, resultCount, filter) => {
+  const url = 'http://flightxml.flightaware.com/json/FlightXML2/' + 'Scheduled'
+  const options = {
+    url: url,
+    params: {
+      airport: airport,
+      howMany:resultCount,
+      filter: filter
+    },
+    auth
+  }
+  const result = await axios(options)
+  return result.data.ScheduledResult.scheduled
+}
+
+const queryFlightInfo = async (tailNumber, resultCount) => {
+  const url = 'http://flightxml.flightaware.com/json/FlightXML2/' + 'FlightInfoEx'
+  const options = {
+    url: url,
+    params: {
+      ident: tailNumber,
+      howMany:resultCount,
+      offset: 0
+    },
+    auth
+  }
+  const result = await axios(options)
+  return result.data.FlightInfoExResult.flights
+}
+
+module.exports = {
+  queryFlightInfo: queryFlightInfo,
+  queryAirportScheduled: queryAirportScheduled
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-// const queryAirportScheduled = require('./APIs/flightAwareAPI').queryAirportScheduled
-// const queryFlightInfo = require('./APIs/flightAwareAPI').queryFlightInfo
-// const geocode = require('./APIs/googleMapsAPI').getGeocode
+const queryAirportScheduled = require('./APIs/flightAwareAPI').queryAirportScheduled
+const queryFlightInfo = require('./APIs/flightAwareAPI').queryFlightInfo
+const geocode = require('./APIs/googleMapsAPI').getGeocode
 
 const app = express()
 
@@ -9,12 +9,12 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' })
 })
 
-// queryFlightInfo('JQ291', 15).then(flights => {
-//   flights.map(flight => {
-//     const departureTime = new Date(flight.filed_departuretime * 1000)
-//     geocode(flight.origin).then(x => console.log(x))
-//   })
-// })
+queryFlightInfo('JQ291', 15).then(flights => {
+  flights.map(flight => {
+    const departureTime = new Date(flight.filed_departuretime * 1000)
+    geocode(flight.origin).then(x => console.log(x))
+  })
+})
 
 const PORT = (process.env.PORT || 5000)
 app.listen(PORT)

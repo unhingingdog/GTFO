34de5c39e6f016c0459b1a1d67c01f0b91ad5440
HAMISH <constatcopywriting@gmail.com>
2018-07-17 21:19:03 +1200

    Refactoring GoogleMaps calls into distanceToAirport function.

--- index.js
+++ index.js
@@ -13,28 +13,29 @@ app.get('/api/get_flight/:tailNumber&:currentLatitude&:currentLongitude',
 async (req, res) => {
   const { tailNumber, currentLatitude, currentLongitude }  = req.params
 
-  // const flights = await queryFlightInfo(tailNumber, 15)
-  // const currentTime = Date.now() / 1000
-  // const upcomingFlights = flights.filter(flight =>
-  //   flight.filed_departuretime > currentTime
-  // )
+  const flights = await queryFlightInfo(tailNumber, 15)
+  const currentTime = Date.now() / 1000
+  const upcomingFlights = flights
+    .filter(flight => flight.filed_departuretime > currentTime)
+    //filter out closest to airport and return duration details
 
-  //getDistance([currentLatitude, currentLongitude], [-41.2784, 174.7767])
+  const distanceToAirport = async (airportCode, currentLatitude, currentLongitude) => {
+    return getDistance(
+      await geocode(airportCode),
+      reverseGeocode([currentLatitude, currentLongitude])
+    )
+  }
 
+  // const airportLocation = await geocode('WGTN')
+  // // console.log('geocode ', airportLocation)
+  //
+  // const currentLocation = await reverseGeocode([currentLatitude, currentLongitude])
+  // // console.log('reverse geocode ', currentLocation)
+  //
+  // const distance = await getDistance(currentLocation, airportLocation)
+  // console.log('DISTANCE: ', distance)
 
-  const airportLocation = await geocode('WGTN')
-  // console.log('geocode ', airportLocation)
-
-  const currentLocation = await reverseGeocode([currentLatitude, currentLongitude])
-  // console.log('reverse geocode ', currentLocation)
-
-  const distance = await getDistance(currentLocation, airportLocation)
-  console.log('DISTANCE: ', distance)
-
-  const directions = await getDirections(currentLocation, airportLocation)
-  console.log('DIRECTIONS: ', directions)
-
-  // res.send(flights)
+  res.send(flights)
 })
 
 const PORT = (process.env.PORT || 5000)
